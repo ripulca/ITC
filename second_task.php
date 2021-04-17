c163abb9a6cd76030738e352c7b5dcb1483ca0f1
ripulca <reneitpa@gmail.com>
2021-04-17 13:46:56 +0300

    ...

--- second_task.php
+++ second_task.php
@@ -1,4 +1,4 @@
-<form method="GET" action="<?=$_SERVER['PHP_SELF']?>">
+<form method="POST" action="<?=$_SERVER['PHP_SELF']?>">
    first_name:<input type="text" name="input1" />
    last_name:<input type="text" name="input2" />
    patronymic:<input type="text" name="input3" />
@@ -6,9 +6,9 @@
    <button type="submit">Send</button>
 </form>
 <?php
-	$input_1=$_GET['input1'];
-	$input_2=$_GET['input2'];
-	$input_3=$_GET['input3'];
+	$input_1=$_POST['input1'];
+	$input_2=$_POST['input2'];
+	$input_3=$_POST['input3'];
 	function concat($first_name, $last_name, $patronymic){
 		echo 'result(last_name first_name patronymic): '.$last_name.' '.$first_name.' '.$patronymic;
 	}
