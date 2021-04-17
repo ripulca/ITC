a71322399e2de3593e57cd47176d68d1d15d27ba
ripulca <reneitpa@gmail.com>
2021-04-17 12:44:32 +0300

    end

--- fourth_task.php
+++ fourth_task.php
@@ -1,7 +1,11 @@
+<form method="GET" action="<?=$_SERVER['PHP_SELF']?>">
+   input:<input type="text" name="input" />
+</form>
 <?php
+	$input=$_GET['input'];
 	function reverse($str){
 		return strrev($str);
 	}
-	$res=reverse("i love food:)");
-	echo $res;
+	$res=reverse($input);
+	echo 'output: '.$res;
 ?>
