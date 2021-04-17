116f60dcc5cb8451dac3519e2a69ae66a3775524
ripulca <reneitpa@gmail.com>
2021-04-17 12:44:45 +0300

    end

--- first_task.php
+++ first_task.php
@@ -1,8 +1,11 @@
+<form method="GET" action="<?=$_SERVER['PHP_SELF']?>">
+   <input type="text" name="input" />
+</form>
 <?php
-	$input=mt_rand();
+	$input=$_GET['input'];
 	function secInDays($days){
-		echo $days."\n";
+		echo "days: ".$days."\n";
 		return $days*86400;
 	}
-	echo secInDays($input);
+	echo 'seconds in days: '.secInDays($input);
 ?>
