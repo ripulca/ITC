3a40167415c18faaa5c5cbf1bf8260f7e5cbe10f
ripulca <reneitpa@gmail.com>
2021-04-25 21:36:34 +0300

    /

--- GTO/GTO_1_3.php
+++ GTO/GTO_1_3.php
@@ -2,19 +2,29 @@
    <input type="text" name="number" />
 </form>
 <?php 
-	$num=$_GET['number'];
-	if (1<=($num / 1000) && ($num / 1000)<10) {
-		$arr=array();
-		for($i=10**3;$i>=1;$i/=10){
-			array_push($arr, floor($num / $i));
-			$num%=$i;
+	if(isset($_GET['number'])){
+		$num=$_GET['number'];
+		if(!is_numeric($num)){
+			echo "gg wp";
+			return;
 		}
-		arsort($arr);
-		foreach ($arr as $value) {
-		 	echo $value;
-		 }
-	}
-	else{
-		echo 'enter a four-digit number';
+		if(!is_int($num=+$num)){
+			echo "gg wp";
+			return;
+		}
+		if (1<=($num / 1000) && ($num / 1000)<10) {
+			$arr=array();
+			for($i=10**3;$i>=1;$i/=10){
+				array_push($arr, floor($num / $i));
+				$num%=$i;
+			}
+			arsort($arr);
+			foreach ($arr as $value) {
+			 	echo $value;
+			 }
+		}
+		else{
+			echo 'enter a four-digit number';
+		}
 	}
  ?>
