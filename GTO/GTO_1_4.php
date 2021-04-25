9c26ba46ceba0752eba4cbc47469b0cc44a0a5fb
ripulca <reneitpa@gmail.com>
2021-04-25 21:44:17 +0300

    /

--- GTO/GTO_1_4.php
+++ GTO/GTO_1_4.php
@@ -2,10 +2,19 @@
    path from home to first shop:<input type="text" name="path_1" />
    path from home to second shop:<input type="text" name="path_2" />
    path from first shop to second shop:<input type="text" name="path_3" />
+   <button type="submit">Send</button>
 </form>
 <?php 
-	$path_1=$_GET['path_1'];
-	$path_2=$_GET['path_2'];
-	$path_3=$_GET['path_3'];
-	echo 'minimal path: '.min($path_1+$path_2+$path_3,2*($path_1+$path_2),2*($path_1+$path_3),2*($path_2+$path_3));
+	if(isset($_GET['path_1'])&& isset($_GET['path_2'])&& isset($_GET['path_3'])){
+		$path_1=$_GET['path_1'];
+		$path_2=$_GET['path_2'];
+		$path_3=$_GET['path_3'];
+		if (!(is_numeric($path_1)&& is_numeric($path_2)&& is_numeric($path_3))) {
+			echo "gg wp";
+			return;
+		}
+		if(is_int($path_1=+$path_1)&& is_int($path_2=+$path_2)&& is_int($path_3=+$path_3)){
+			echo 'minimal path: '.min($path_1+$path_2+$path_3,2*($path_1+$path_2),2*($path_1+$path_3),2*($path_2+$path_3));
+		}
+	}
  ?>
