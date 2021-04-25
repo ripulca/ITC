c6d8d4b31a4e388d03170ff6ed9674b4ab5ea80d
ripulca <reneitpa@gmail.com>
2021-04-25 21:36:24 +0300

    /

--- GTO/GTO_1_2.php
+++ GTO/GTO_1_2.php
@@ -2,18 +2,27 @@
    rubls:<input type="text" name="rubls" />
    cop:<input type="text" name="cop" />
    amount of buns:<input type="text" name="buns" />
+   <button type="submit">Send</button>
 </form>
 <?php 
-	$rub=$_GET['rubls'];
-	$cop=$_GET['cop'];
-	$buns=$_GET['buns'];
-	function calcThePrice($rub, $cop, $buns){
-		$result_rub=$rub*$buns;
-		if(($result_cop=$cop*$buns)>=100){
-			$result_rub++;
-			$result_cop-=100;
+	if(isset($_GET['rubls'])&& isset($_GET['cop'])&& isset($_GET['buns'])){
+		$rub=$_GET['rubls'];
+		$cop=$_GET['cop'];
+		$buns=$_GET['buns'];
+		if (!(is_numeric($rub)&& is_numeric($cop)&& is_numeric($buns))) {
+			echo "gg wp";
+			return;
 		}
-		echo 'Amount of buns: '.$buns.' Total price: rubles: '.$result_rub.' kopek: '.$result_cop;
+		if(is_int($rub=+$rub)&& is_int($cop=+$cop)&& is_int($buns=+$buns)){
+			function calcThePrice($rub, $cop, $buns){
+				$result_rub=$rub*$buns;
+				if(($result_cop=$cop*$buns)>=100){
+					$result_rub++;
+					$result_cop-=100;
+				}
+				echo 'Amount of buns: '.$buns.' Total price: rubles: '.$result_rub.' kopek: '.$result_cop;
+			}
+			calcThePrice($rub, $cop, $buns);
+		}
 	}
-	calcThePrice($rub, $cop, $buns);
 ?>
