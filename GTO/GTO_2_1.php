a3954ba512b9c49fdecc9e19e749e3ee498366ac
ripulca <reneitpa@gmail.com>
2021-04-25 21:44:29 +0300

    /

--- GTO/GTO_2_1.php
+++ GTO/GTO_2_1.php
@@ -2,19 +2,29 @@
    <input type="text" name="number" />
 </form>
 <?php 
-	$max_num=$_GET['number'];
-	if(0<$max_num && $max_num<10**6){
-		foreach (range(0, $max_num) as $num) {
-			$counter=10;
-			while ($num>$counter) {
-				$counter*=10;
-			}
-			if((($num**2) % $counter) == $num){
-				echo $num.' ';
+	if(isset($_GET['number'])){
+		$max_num=$_GET['number'];
+		if(!is_numeric($num)){
+			echo "gg wp";
+			return;
+		}
+		if (!is_int($num=+$num)) {
+			echo "gg wp";
+			return;
+		}	
+		if(0<$max_num && $max_num<10**6){
+			foreach (range(0, $max_num) as $num) {
+				$counter=10;
+				while ($num>$counter) {
+					$counter*=10;
+				}
+				if((($num**2) % $counter) == $num){
+					echo $num.' ';
+				}
 			}
 		}
-	}
-	else{
-		echo 'enter a number less than 10^6';
+		else{
+			echo 'enter a number less than 10^6';
+		}
 	}
  ?>
