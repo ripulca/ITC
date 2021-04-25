d4bafeebe6e8bb86580bdb7245133fa656cda061
ripulca <reneitpa@gmail.com>
2021-04-25 21:45:44 +0300

    /

--- GTO/GTO_2_3.php
+++ GTO/GTO_2_3.php
@@ -2,24 +2,34 @@
    <input type="text" name="number" />
 </form>
 <?php 
-	$num=$_GET['number'];
-	if(2<=$num && $num<=2*10**5){
-		function is_primal($num){
-			if($num==2 || $num==3){
-				return "prime";
-			}
-			else{
-				for($i=1;$i<=floor(sqrt($num));$i++){
-					if(($num%$i)==0 && $i!=1 && $i!=$num){
-						return "composite";
-					}
-				}
-				return "prime";
-			}
+	if(isset($_GET['number'])){
+		$num=$_GET['number'];
+		if(!is_numeric($num)){
+			echo "gg wp";
+			return;
 		}
-		echo is_primal($num);
-	}
-	else{
-		echo "enter integer number that bigger than 2";
+		if (!is_int($num=+$num)) {
+			echo "gg wp";
+			return;
+		}
+		if(2<=$num && $num<=2*10**5){
+			function is_primal($num){
+				if($num==2 || $num==3){
+					return "prime";
+				}
+				else{
+					for($i=1;$i<=floor(sqrt($num));$i++){
+						if(($num%$i)==0 && $i!=1 && $i!=$num){
+							return "composite";
+						}
+					}
+					return "prime";
+				}
+			}
+			echo is_primal($num);
+		}
+		else{
+			echo "enter integer number that bigger than 2";
+		}
 	}
 ?>
